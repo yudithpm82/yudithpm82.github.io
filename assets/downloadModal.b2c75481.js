e413d3d8ead015ac4a94c4a9144082682d3a32a4
Hector Ricardom <hectoricardom@gmail.com>
2022-08-16 19:01:28 -0400

    hhyg897699yi7

---
+++ assets/downloadModal.b2c75481.js
@@ -0,0 +1,6 @@
+import{I as o,r as d,k as r,j as i,a as e}from"./index.4b9c9f70.js";const p=o(),u=l=>{const{data:s}=l,{modalID:a}=s,[n,c]=d.exports.useState(!1);return d.exports.useEffect(()=>{n||c(!0)},[]),r("userProfile"),i("div",{...a?{"dialog-key-id":a}:"",className:"pym81b sendBx upd_alert_bx",style:{background:"var(--color_backgrond_input)"},children:[e("style",{children:`
+        .palette{
+          --base-color: rgb(21, 100, 191,1);
+          --base-color-gradient: 21, 100, 191;
+        }
+        `}),e("div",{className:" _dsplFlx spaceAround",children:e("div",{className:"title_download_modal ",children:"Descargar aplicaciones"})}),e("div",{className:" update_ftp _dsplFlx spaceAround",children:e(t,{lbl:"Android",icon:"tablet_ipad",theme:"",updChanges:()=>window.open("/getStatic/hlgflix.apk")})}),e("div",{className:" update_ftp _dsplFlx spaceAround",children:e(t,{lbl:"Android TV",icon:"tv_app",theme:"",updChanges:()=>window.open("/getStatic/tv.hlgflix.apk")})})]})},t=({icon:l,lbl:s,updChanges:a})=>e("div",{className:"cardDown green",children:i("div",{className:"_dsplFlx",onClick:a,children:[e("div",{className:"icon_ds",children:e(p,{name:l})}),e("div",{className:"lbl",children:` ${s} `})]})});export{u as default};
