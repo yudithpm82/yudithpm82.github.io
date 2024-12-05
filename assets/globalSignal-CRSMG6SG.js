6a1aa1ce1ab4aa41759f3b74ae3931005fde7078
Hector Ricardo <hectoricardom@gmail.com>
2024-12-05 09:41:14 -0500

    juhygtf56gy7hunim

---
+++ assets/globalSignal-CRSMG6SG.js
@@ -0,0 +1,1 @@
+import{a,c as o}from"./index-BFj7Mech.js";const[e,i]=a(!0);setInterval(()=>{let t=720;e()!==window.outerWidth<t&&i(window.outerWidth<t)},220);const[s,l]=o({});o({});export{e as i,l as s};
