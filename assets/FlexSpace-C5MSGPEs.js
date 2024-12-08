5f3d4ee8fc6c7d201ab729b6ff13f5f24015edda
Hector Ricardo <hectoricardom@gmail.com>
2024-12-08 09:43:30 -0500

    jnbhyv67b8in

---
+++ assets/FlexSpace-C5MSGPEs.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CvYaQ-s6.js";var t=e("<div class=flexSpace>");const l=()=>t();export{l as default};
---
+++ assets/globalSignal-BQY5JgUu.js
@@ -0,0 +1,1 @@
+import{a,c as o}from"./index-CvYaQ-s6.js";const[e,i]=a(!0);setInterval(()=>{let t=720;e()!==window.outerWidth<t&&i(window.outerWidth<t)},220);const[s,l]=o({});o({});export{e as i,l as s};
